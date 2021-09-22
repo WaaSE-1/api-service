7d03fe893d40e4cdc705d7ece818989e5bc48a1d
Toms Audrins <toms.audrins@gmail.com>
2021-09-22 07:39:49 +0200

    Add user saving into database functionality.

--- src/modules/mysql/db.py
+++ src/modules/mysql/db.py
@@ -5,23 +5,28 @@ db_details = {
     "user": "cardealershipadmin",
     "password": "nJi0HePX1DVdX8bSnXIEjc0S",
     "database": "cardealership",
-    "port": 3306
+    "port": 3306,
 }
+
 
 class DBConnection:
     def __init__(self):
         self.conn = connect(**db_details)
         self.cursor = self.conn.cursor()
 
-    def create_user():
-        pass
+    def create_user(self, user):
+        insert_query = f"""
+            INSERT INTO customer (firstname, lastname, email, phone_number, location_id, address, password)
+            VALUES ('{user.firstname}', '{user.lastname}', '{user.email}', '{user.phonenumber}', '{user.loc_id}', '{user.address}', {user.password});
+        """
+        self.cursor.execute(insert_query)
+        self.conn.commit()
+        return 200
 
     def find_user(email: str):
         return {
             "id": 1,
-            "name":"Rick Astley",
+            "name": "Rick Astley",
             "email": "toms@gmail.com",
-            "password":"argon2"
+            "password": "argon2",
         }
-
-
