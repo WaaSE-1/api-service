90e718b5f7c0c6c4ab075f79c01c25f568f9b71e
Toms Audrins <toms.audrins@gmail.com>
2021-09-24 07:16:42 +0200

    Add response with status codes to register route

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-from fastapi import FastAPI
-
-# from fastapi import FastAPI, Depends, Request, HTTPException
-# from fastapi.security.api_key import APIKey
-from src.schema.user import User
-from argon2 import PasswordHasher
-from src.modules.mysql.db import DBConnection
 import re
+from argon2 import PasswordHasher
+from fastapi import FastAPI, Response, status
+from src.modules.mysql.db import DBConnection
+from src.schema.user import User
 
 app = FastAPI()
 
 
 # Request to register a user
-@app.post("/register")
-async def register_user(User: User):
+@app.post("/register", status_code=201)
+async def register_user(User: User, Response: Response):
+
     db = DBConnection()
     # Check if the password matches the criteria
     if not re.match(r"[A-Za-z0-9@#$%^&+=]{8,32}", User.password):
-        return False
+        Response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"error": "Password does not meet the set criteria!"}
 
-    if db.find_user_by_email(User.email) is None:
-        # Store the hashed password in the User object.
-        User.password = PasswordHasher().hash(User.password)
-        db.create_user(User)
-    else:
-        return False
+    # Check if the user account already exists
+    if db.find_user_by_email(User.email) is not None:
+        Response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"error": "User with this email address already exists!"}
+
+    # Store the hashed password in the User object.
+    User.password = PasswordHasher().hash(User.password)
+    return db.create_user(User)
 
 
 # Request to verify a user
@@ -42,6 +43,7 @@ async def login_user(email: str, password: str):
         return True
     except:
         return False
+
 
 @app.post("/delete")
 async def delete_user(email: str):
