91ef4f018dde31912077c055ba41259a36c7d2ce
jacobsibbern <jacob@sibbern.dk>
2021-10-02 15:38:05 +0200

    Added token expire datetime
    Co-authored-by: Tom <tomsaudrins@users.noreply.github.com>

--- src/modules/auth/core.py
+++ src/modules/auth/core.py
@@ -1,19 +1,20 @@
 
 import jwt
 from fastapi.security import OAuth2PasswordBearer,  SecurityScopes
-from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from pydantic import ValidationError
-from src.modules.mysql import DBConnection
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+
 
 # Local packages
+from src.modules.mysql import DBConnection
 from src.settings.envvariables import Settings
-from src.schema import auth
+
 
 
 class Auth:
     
-    # FastAPI Example: https://fastapi.tiangolo.com/advanced/security/oauth2-scopes/
     # Used by adding token: str = Depends(Auth.validate_token) as a parameter
     
     oauth2_scheme = OAuth2PasswordBearer(
@@ -28,6 +29,7 @@ class Auth:
         security_scopes: SecurityScopes, token: str = Depends(oauth2_scheme)):
         
         authenticate_value = f"Bearer"
+        
         credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token",
@@ -37,14 +39,16 @@ class Auth:
         # Validate the token by finding the user and generating the token
         try:
             payload = jwt.decode(token, Settings().JWT_SECRET, algorithms="HS256")
-            username: str = payload.get("email")
-            if username is None:
+            email: str = payload.get("email")
+            token_expires_datetime = datetime.strptime(payload.get('expires'), '%Y-%m-%d %H:%M:%S')
+        
+            if token_expires_datetime < datetime.now() or email is None:
                 raise credentials_exception
-            token_data = auth.TokenData(username=username)
+        
         except (jwt.PyJWTError, ValidationError):
             raise credentials_exception
         
-        user = DBConnection().find_user_by_email(email=username) 
+        user = DBConnection().find_user_by_email(email=email) 
         
         if user is None:
             raise credentials_exception
@@ -52,10 +56,11 @@ class Auth:
 
         return user
     
-
-    def create_token(user):
-        del user["password"]
-        token = jwt.encode(user, Settings().JWT_SECRET, algorithm="HS256")
+    # Create a token to authenticate the user
+    def create_token(user, expires_delta=1440):
+        to_encode = user.copy()
+        del to_encode["password"]
+        to_encode.update({"expires": (datetime.now() + timedelta(minutes=expires_delta)).__str__()[0:-7]})
+        print(to_encode)
+        token = jwt.encode(to_encode, Settings().JWT_SECRET, algorithm="HS256")
         return dict(access_token=token, token_type="bearer")
-
-
