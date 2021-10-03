f3f4bd54753d13e2a2e431684efb5ab4a67577c5
Toms Audrins <toms.audrins@gmail.com>
2021-10-03 10:20:28 +0200

    Include registration, Unauthorized delete, Authorized delete tests

--- test_users.py
+++ test_users.py
@@ -6,6 +6,19 @@ client = TestClient(app)
 
 token: str
 
+def test_register_success():
+    user = {
+        "firstname": "Tue",
+        "lastname": "Hellstern",
+        "email": "testlogin@mail.com",
+        "phonenumber": "2123222",
+        "loc_id": 2200,
+        "address": "Guldbergsgade 29N",
+        "password": "1234Tecc1"
+    }
+    response = client.post("/users/register", json=user)
+    assert response.status_code == 201
+
 def test_login_success():
     user = {
         "email": "testlogin@mail.com",
@@ -32,3 +45,26 @@ def test_login_password_incorrect():
     response = client.post("/users/login", json=user)
     assert 'error' in response.json()
     assert response.status_code == 401
+
+
+def test_user_delete_unauthorized():
+    user = {
+        "email": "testlogin@mail.com",
+        "password": "1"
+    }   
+    response = client.delete("/users/delete", json=user)
+    #token = response.json()['token']['access_token']
+    assert response.status_code == 401
+
+def test_user_delete_authorized():
+    user = {
+        "email": "testlogin@mail.com",
+        "password": "1234Tecc1"
+    }   
+    response = client.post("/users/login", json=user)
+    token = response.json()['token']['access_token']
+    user = {
+        "email": "testlogin@mail.com"
+    }
+    response = client.delete("/users/delete", json=user, headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
