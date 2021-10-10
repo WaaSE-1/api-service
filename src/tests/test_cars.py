663e0d56ec94c016ad5f2e8ee77014fd966c4756
Toms Audrins <toms.audrins@gmail.com>
2021-10-10 18:22:36 +0200

    Create test for creating a new car

--- src/tests/test_cars.py
+++ src/tests/test_cars.py
@@ -1,8 +1,31 @@
 from fastapi.testclient import TestClient
 from src.routes.car import app
+from src import DBConnection
 
 client = TestClient(app)
+
 
 def test_car_get_all():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_car_add_new():
+    db = DBConnection()
+    db.cursor.execute(
+        'DELETE from vehicle WHERE id = (select * from (select id from vehicle vh where model = "TestModelOnly")tbltemp)'
+    )
+    db.conn.commit()
+    car = {"manufacturer": "BMW", "model": "TestModelOnly", "year": 2100, "price": 1242}
+    response = client.post("/", json=car)
+    assert response.status_code == 201
+    db.cursor.execute(
+        'SELECT manufacturer_id, model, year, price FROM vehicle WHERE model="TestModelOnly"'
+    )
+    for i in db.cursor.fetchall():
+        assert i == {
+            "manufacturer_id": 5,
+            "model": "TestModelOnly",
+            "year": 2100,
+            "price": 1242,
+        }
