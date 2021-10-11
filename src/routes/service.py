14dc1f6dd5a62761296d7604ec6eb4afef69469e
Stolten <b.skovhoj@gmail.com>
2021-10-11 21:10:01 +0200

    Add route for service requests

---
+++ src/routes/service.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter  # , Response
+from src.modules.mysql.db import DBConnection
+from src.schema import service
+
+app = APIRouter()
+
+# Create service request
+@app.post("/", status_code=201)
+async def create_service_request(service: service.Service):
+    db = DBConnection()
+    return db.create_service_request(service.dict())
+
