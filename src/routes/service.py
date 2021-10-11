20e1f40e00a79dfe5b6e9e94ff2e52ed3ec2c25b
Stolten <b.skovhoj@gmail.com>
2021-10-11 21:14:20 +0200

    Add documentation for 'create_service_request'

--- src/routes/service.py
+++ src/routes/service.py
@@ -7,6 +7,7 @@ app = APIRouter()
 # Create service request
 @app.post("/", status_code=201)
 async def create_service_request(service: service.Service):
+    # Establishes connection to the database
     db = DBConnection()
     return db.create_service_request(service.dict())
 
