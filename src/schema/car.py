b3f1a4153e3716c079d7fe6b71b98e9763f36d90
Crap-Head <sebastian7307@hotmail.com>
2021-10-12 22:10:15 +0200

    Little comment ups fix

--- src/schema/car.py
+++ src/schema/car.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
 
-# Define classes for a car and deleting a car
+# Define class for a car (creating)
 class Car(BaseModel):
     manufacturer: str
     model: str
     year: int
     price: float
 
-
+# Define class for deleting a car
 class Delete(BaseModel):
     vehicle: int
     dealership: str
