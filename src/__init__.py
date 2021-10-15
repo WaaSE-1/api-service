5f88ee96e6a4b068be65614709a00b3bde7e17ee
jacobsibbern <jacob@sibbern.dk>
2021-10-15 11:18:35 +0200

    Added pytest to Dockerfile

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,5 @@ from src.settings.envvariables import Settings
 from src.modules.mysql.db import DBConnection
 from src.routes import user
 from src import schema
+
+from .tests import test_cars, test_product, test_user
